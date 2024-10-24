f3f4b96442b88ca8527d7768bc2a4eddaf9179d2
prokawsar <prokawsar@gmail.com>
2024-10-24 11:10:55 +0600

    Init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	let count = $state(0)
+
+	const computed = $derived(count * 2)
+
+	$effect(() => {
+		console.log('on mount')
+	})
+</script>
+
+<div class="mx-auto flex h-screen max-w-7xl items-center justify-center text-xl font-bold">
+	<button class="cursor-pointer border px-10 py-6" onclick={() => count++}>{count}</button>
+	<p class="cursor-pointer border px-10 py-6">{computed}</p>
+</div>
